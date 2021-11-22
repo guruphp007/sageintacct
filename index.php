fb980ae08ad12725f52dbda124072a0f1a447b1a
Guruprabakaran <careers.guruprabakaran@gmail.com>
2021-11-22 20:11:22 +0530

    13 commit

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: X-Requested-With");
 
 require "BaseController.php";
 require "BaseModel.php";
